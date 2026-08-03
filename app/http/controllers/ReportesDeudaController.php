959716d1e81391cb1a39bfb4abb05f3d5746b04c
victorraulcancah <vcanchari38@gmail.com>
2026-08-03 10:21:02 -0500

    Ajustar la tabla de reporte de clientes para incluir el número de teléfono y mejorar la presentación

--- app/http/controllers/ReportesDeudaController.php
+++ app/http/controllers/ReportesDeudaController.php
@@ -555,7 +555,7 @@ class ReportesDeudaController extends Controller
         $mercadoActual = $mercado;
         $rowTable .= "
           <tr>
-            <td colspan='3' style='text-align:left;background:#e9e9e9;font-weight:bold;padding:4px 6px;'>MERCADO: {$mercado}</td>
+            <td colspan='4' style='text-align:left;background:#e9e9e9;font-weight:bold;padding:4px 6px;'>MERCADO: {$mercado}</td>
           </tr>
         ";
       }
@@ -567,14 +567,15 @@ class ReportesDeudaController extends Controller
       $rowTable .= "
         <tr>
           <td style='text-align:center;'>{$n}</td>
-          <td style='text-align:left;'>{$nombre}<br><span style='font-size:9px;color:#555;'>{$documento} · {$telefono}</span></td>
+          <td style='text-align:left;'>{$nombre}<br><span style='font-size:9px;color:#555;'>{$documento}</span></td>
           <td style='text-align:left;'>{$direccion}</td>
+          <td style='text-align:center;'>{$telefono}</td>
         </tr>
       ";
     }
 
     if ($rowTable === '') {
-      $rowTable = "<tr><td colspan='3' style='text-align:center;padding:12px;'>No se encontraron clientes para el filtro seleccionado.</td></tr>";
+      $rowTable = "<tr><td colspan='4' style='text-align:center;padding:12px;'>No se encontraron clientes para el filtro seleccionado.</td></tr>";
     }
 
     $html = "
@@ -589,9 +590,10 @@ class ReportesDeudaController extends Controller
             <table border='1' style='width: 100%; text-align: center; border-collapse: collapse; font-size:11px;'>
                 <thead>
                   <tr style='background:#d0d0d0;'>
-                      <th style='padding: 3px 6px; width:8%;'>N°</th>
-                      <th style='padding: 3px 6px; width:52%;'>CLIENTE</th>
-                      <th style='padding: 3px 6px; width:40%;'>DIRECCIÓN</th>
+                      <th style='padding: 3px 6px; width:6%;'>N°</th>
+                      <th style='padding: 3px 6px; width:42%;'>CLIENTE</th>
+                      <th style='padding: 3px 6px; width:35%;'>DIRECCIÓN</th>
+                      <th style='padding: 3px 6px; width:17%;'>TELÉFONO</th>
                   </tr>
                 </thead>
                 <tbody>
